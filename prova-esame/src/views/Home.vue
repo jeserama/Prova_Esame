12217ef3df8a70506f9a70de51d824f61c636777
Miharinjato Jese Ramanantsoa <jese.ramanantsoa@stud.tecnicosuperiorekennedy.it>
2021-06-24 14:55:04 +0200

    Home

--- prova-esame/src/views/Home.vue
+++ prova-esame/src/views/Home.vue
@@ -1,8 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div class="container-fluid">
+    <div class="row">
+      <img alt="ski image" src="../assets/baner.jpg">
+      <div class="centered">Benvenuti a SKI FVG</div>
+    </div>
+    
+
+      
+        <div class="card mt-5">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/baner.jpg" alt="ski image">
+                    
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h4 class="card-title">Pianasino</h4>
+                        <p class="card-text"> la prima stazione sciistica italiana a dotarsi di un sistema di innevamento artificiale. Ospitò dal 1979 agli inizi degli anni novanta varie gare femminili di Coppa del Mondo di sci alpino. Dal 1970 vi si è svolto per molti anni il Rally Piancavallo, di valenza europea; ora si svolge annualmente un rally di livello nazionale. Ha ospitato l'arrivo della quattordicesima tappa Giro d'Italia del 1998, in cui si impose Marco Pantani, e della diciannovesima tappa del Giro d'Italia 2017 vinta da Mikel Landa. Inoltre il Giro vi transitò nella quindicesima tappa del Giro d'Italia del 2011, dove scollinò per primo Emanuele Sella.</p>
+                        <a href="#" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-5">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/baner.jpg" alt="ski image">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h4 class="card-title">Pianasino</h4>
+                        <p class="card-text"> la prima stazione sciistica italiana a dotarsi di un sistema di innevamento artificiale. Ospitò dal 1979 agli inizi degli anni novanta varie gare femminili di Coppa del Mondo di sci alpino. Dal 1970 vi si è svolto per molti anni il Rally Piancavallo, di valenza europea; ora si svolge annualmente un rally di livello nazionale. Ha ospitato l'arrivo della quattordicesima tappa Giro d'Italia del 1998, in cui si impose Marco Pantani, e della diciannovesima tappa del Giro d'Italia 2017 vinta da Mikel Landa. Inoltre il Giro vi transitò nella quindicesima tappa del Giro d'Italia del 2011, dove scollinò per primo Emanuele Sella.</p>
+                        <a href="#" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-5">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/baner.jpg" alt="ski image">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h4 class="card-title">Pianasino</h4>
+                        <p class="card-text"> la prima stazione sciistica italiana a dotarsi di un sistema di innevamento artificiale. Ospitò dal 1979 agli inizi degli anni novanta varie gare femminili di Coppa del Mondo di sci alpino. Dal 1970 vi si è svolto per molti anni il Rally Piancavallo, di valenza europea; ora si svolge annualmente un rally di livello nazionale. Ha ospitato l'arrivo della quattordicesima tappa Giro d'Italia del 1998, in cui si impose Marco Pantani, e della diciannovesima tappa del Giro d'Italia 2017 vinta da Mikel Landa. Inoltre il Giro vi transitò nella quindicesima tappa del Giro d'Italia del 2011, dove scollinò per primo Emanuele Sella.</p>
+                        <a href="#" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-5">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/baner.jpg" alt="ski image">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h4 class="card-title">Pianasino</h4>
+                        <p class="card-text"> la prima stazione sciistica italiana a dotarsi di un sistema di innevamento artificiale. Ospitò dal 1979 agli inizi degli anni novanta varie gare femminili di Coppa del Mondo di sci alpino. Dal 1970 vi si è svolto per molti anni il Rally Piancavallo, di valenza europea; ora si svolge annualmente un rally di livello nazionale. Ha ospitato l'arrivo della quattordicesima tappa Giro d'Italia del 1998, in cui si impose Marco Pantani, e della diciannovesima tappa del Giro d'Italia 2017 vinta da Mikel Landa. Inoltre il Giro vi transitò nella quindicesima tappa del Giro d'Italia del 2011, dove scollinò per primo Emanuele Sella.</p>
+                        <a href="#" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-5">
+            <div class="row no-gutters">
+                <div class="col-sm-5">
+                    <img class="card-img" src="../assets/baner.jpg" alt="ski image">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <h4 class="card-title">Pianasino</h4>
+                        <p class="card-text"> la prima stazione sciistica italiana a dotarsi di un sistema di innevamento artificiale. Ospitò dal 1979 agli inizi degli anni novanta varie gare femminili di Coppa del Mondo di sci alpino. Dal 1970 vi si è svolto per molti anni il Rally Piancavallo, di valenza europea; ora si svolge annualmente un rally di livello nazionale. Ha ospitato l'arrivo della quattordicesima tappa Giro d'Italia del 1998, in cui si impose Marco Pantani, e della diciannovesima tappa del Giro d'Italia 2017 vinta da Mikel Landa. Inoltre il Giro vi transitò nella quindicesima tappa del Giro d'Italia del 2011, dove scollinò per primo Emanuele Sella.</p>
+                        <a href="#" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+   
+
+
+</div>
+  
+
 </template>
 
 <script lang="ts">
@@ -16,3 +99,16 @@ export default defineComponent({
   },
 });
 </script>
+<style>
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 60px;
+  color:azure;
+}
+.btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary:visited {
+    background-color: #03DAC6 !important;
+}
+</style>
