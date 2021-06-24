54cd5a0db22600698b5953ec8e2e61f633a2bdee
Miharinjato Jese Ramanantsoa <jese.ramanantsoa@stud.tecnicosuperiorekennedy.it>
2021-06-24 14:09:39 +0200

    add dependencies

--- prova-esame/src/main.ts
+++ prova-esame/src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faPhone);
+const app = createApp(App);
+app
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app")
